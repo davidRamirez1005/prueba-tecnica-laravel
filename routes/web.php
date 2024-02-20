e1aa311e3c05e905b36e13a94f4294ee4894a49f
davidRamirez1005 <ramirezjd1005@gmail.com>
2024-02-20 17:31:09 -0500

    feat:

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/angular-app/{any}', function () {
+    return view('angular-app');
+})->where('any', '.*');
